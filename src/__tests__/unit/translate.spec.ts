24ef234c62237e2eef6c9efd9ceac65b1019f3ec
Ryan Sonshine <ryansonshine@users.noreply.github.com>
2022-11-18 09:23:30 +0700

    test(translate): add test case for absolute path

--- src/__tests__/unit/translate.spec.ts
+++ src/__tests__/unit/translate.spec.ts
@@ -1,6 +1,52 @@
-import { getSrtPath } from '../../translate';
+const mockTranslateSubtites = jest.fn();
+
+jest.mock('amazon-translate-subtitles', () => ({
+  translateSubtitles: mockTranslateSubtites,
+}));
+
+import tempy from 'tempy';
+import fs from 'fs';
+import { getSrtPath, main } from '../../translate';
+import { Options } from '../../cli';
 
 describe('translate', () => {
+  describe('main', () => {
+    const defaultOptions: Options = {
+      debug: false,
+      input: '',
+      progress: true,
+      targetLanguage: 'auto',
+    };
+
+    let createReadStreamSpy = jest.spyOn(fs, 'createReadStream');
+
+    beforeEach(() => {
+      jest.resetAllMocks();
+      createReadStreamSpy = jest.spyOn(fs, 'createReadStream');
+      jest
+        .spyOn(fs.promises, 'writeFile')
+        .mockImplementation((): Promise<void> => Promise.resolve());
+      mockTranslateSubtites.mockResolvedValue('');
+    });
+
+    it('should resolve an absolute path for input', async () => {
+      await tempy.file.task(
+        async fileName => {
+          await fs.promises.writeFile(fileName, 'test data');
+
+          await main({
+            ...defaultOptions,
+            input: fileName,
+            targetLanguage: 'th',
+          });
+
+          expect(createReadStreamSpy).toHaveBeenCalledWith(fileName);
+        },
+        { extension: 'mkv' }
+      );
+    });
+  });
+
   describe('getSrtPath', () => {
     it('should return an srt path next to the initial video with the language code in the filename', () => {
       const videoPath = '/home/ryansonshine/my-video.mp4';
